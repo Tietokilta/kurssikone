5c9f0e0a8be1d68ece9e8ec4c109a94d7dbd1fe1
Otto Söderman <ottoj.soderman@gmail.com>
2023-08-15 23:32:35 +0300

    Add dev script for faster development

---
+++ src/dev.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+const rootElement = document.getElementById('review-root') as HTMLElement
+const root = ReactDOM.createRoot(rootElement)
+root.render(<App />)
