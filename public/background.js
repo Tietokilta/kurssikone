90975c3bc1d2c28d605e4718ee71c26cf6717bd3
Otju <ottoj.soderman@gmail.com>
2024-01-13 17:49:10 +0200

    Remove useless comments and permissions

--- public/background.js
+++ public/background.js
@@ -1,22 +1,4 @@
 const handleOpenCourse = (details) => {
-  /*
-  let filter = browser.webRequest.filterResponseData(details.requestId)
-  let decoder = new TextDecoder('utf-8')
-  //let encoder = new TextEncoder()
-
-  filter.ondata = (event) => {
-    let str = decoder.decode(event.data)
-    try {
-      const json = JSON.parse(str)
-      console.log(json)
-    } catch (_) {
-      filter.disconnect()
-    }
-    filter.write(event.data)
-  }
-  filter.onstop = () => {
-    filter.disconnect()
-  };*/
   const startOfUrl = 'https://sisu.aalto.fi/osuva/api/notifications/student/aalto-HLO-'
   const url = details.url
   if (details.url.startsWith(startOfUrl)) {
@@ -25,10 +7,6 @@ const handleOpenCourse = (details) => {
   }
 }
 
-browser.webRequest.onBeforeRequest.addListener(
-  handleOpenCourse,
-  {
-    urls: ['*://sisu.aalto.fi/*'],
-  }
-  //['blocking']
-)
+browser.webRequest.onBeforeRequest.addListener(handleOpenCourse, {
+  urls: ['*://sisu.aalto.fi/*'],
+})
