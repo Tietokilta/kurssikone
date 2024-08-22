a38eecbfb27097e9a1f26b2baedddda23192a600
Otju <ottoj.soderman@gmail.com>
2024-08-23 00:04:23 +0300

    Remove useless "dev" stuff

--- src/dev.tsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import CoursePage from './CoursePage'
-
-const rootElement = document.getElementById('review-root') as HTMLElement
-const root = ReactDOM.createRoot(rootElement)
-root.render(<CoursePage />)
