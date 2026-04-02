b40a9ae67b3a015335b375d1a55e3affe08fba40
Otju <ottoj.soderman@gmail.com>
2026-04-03 00:41:00 +0300

    Fix build not working

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@kurssikompassi/shared': path.resolve(__dirname, '../shared/src'),
+    },
   },
 })
